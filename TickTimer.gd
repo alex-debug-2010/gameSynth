f662796af7cc4d2f5ed6327ebd2c0af14c719d70
mikivan1980 <mikivan1980@mail.ru>
2026-07-12 15:45:38 +0300

    add print1

--- TickTimer.gd
+++ TickTimer.gd
@@ -1,10 +1,11 @@
 extends Timer
-print("привет")
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var full_path = self.get_path()
 	print(full_path)
-
+	print("приветnnnnn")
+	
 func _on_timeout():
 	get_node("TickLabel").text = str(int(get_node("TickLabel").text) - 1)
